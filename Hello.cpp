5f2b3b1e293397d64bb6181cddebf0a6833e11f0
Konstantin Bantov <k.doguzov@gmail.com>
2023-01-02 00:39:02 +0300

    Sorry for word idiotic

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+﻿#include "std_lib_facilities.h"
+
+int main() {
+    for (size_t i = 0; i < 6; i++)
+    {
+        for (size_t j = 0; j < 4; j++)
+        {
+            cout << "Hello world!\t";
+        }
+        cout << endl;        
+    }
+    keep_window_open();    
+} 
---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Path is " << fs::path(argv[0]).parent_path() << '\n';
+}
